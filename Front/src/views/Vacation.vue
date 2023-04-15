76b61ea612683d1e590ab2a1a97ece6ffa4d09f4
MrPALpi <kapralov-02@bk.ru>
2023-04-15 20:27:22 +0500

    пулю

---
+++ Front/src/views/Vacation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="vacations-wrap">
+  <div class="vacations-search">
+    
+  </div>
+ <div class="vacation_desk">
+   <VacantionsList/>
+ </div>
+</div>
+</template>
+
+<script>
+import VacantionsList from "@/components/Vacations/VacantionsList.vue.js";
+export default {
+  name: "Vacation",
+  components: {VacantionsList}
+}
+</script>
+
+<style scoped>
+  .vacations-wrap{
+    padding: 50px var(--pad) 0;
+    /* display: flex; */
+  }
+</style>
