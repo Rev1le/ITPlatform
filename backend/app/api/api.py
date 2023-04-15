3b443544fb7e1b5bbcad1521cff58e30a1caab54
Rev1le <r.nikiy@yandex.ru>
2023-04-15 22:46:53 +0500

    Fix

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import auth, registration, task_blocks, update_user_data, vacancy
+from app.api.endpoints import auth, registration, task_blocks, update_user_data, vacancy, mentor
 
 api_router = APIRouter()
 api_router.include_router(registration.router, prefix="/registration")
 api_router.include_router(auth.router, prefix="/auth")
 api_router.include_router(vacancy.router, prefix="/vacancy")
+api_router.include_router(mentor.router, prefix="/mentor")
 api_router.include_router(update_user_data.router, prefix="/update")
 api_router.include_router(task_blocks.router, prefix="/task_block")
