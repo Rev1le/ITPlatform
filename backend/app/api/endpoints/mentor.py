13370e7c4325d53c6cf73425174ce8db2f5d62ae
Gabben <43146729+gabbhack@users.noreply.github.com>
2023-04-15 20:32:45 +0500

    Work

--- backend/app/api/endpoints/mentor.py
+++ backend/app/api/endpoints/mentor.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter
 from app.core.database import edgedb_client
 from app.queries.get_mentors_async_edgeql import GetMentorsResult, get_mentors
 
-
 router = APIRouter()
+
 
 @router.get("/all")
 async def get_all_mentors() -> list[GetMentorsResult]:
--- backend/app/api/endpoints/update_user_data.py
+++ backend/app/api/endpoints/update_user_data.py
@@ -5,12 +5,10 @@ from pydantic import BaseModel
 
 from app.api.deps import check_auth_employer_token, check_auth_worker_token
 from app.core.database import edgedb_client
-
-import app.queries as db_queries
-from db_queries.get_employer_by_token_async_edgeql import GetEmployerByTokenResult
-from db_queries.get_worker_by_token_async_edgeql import GetWorkerByTokenResult
-from db_queries.update_employer_async_edgeql import update_employer
-from db_queries.update_worker_async_edgeql import update_worker
+from app.queries.get_employer_by_token_async_edgeql import GetEmployerByTokenResult
+from app.queries.get_worker_by_token_async_edgeql import GetWorkerByTokenResult
+from app.queries.update_employer_async_edgeql import update_employer
+from app.queries.update_worker_async_edgeql import update_worker
 
 router = APIRouter()
 
@@ -18,7 +16,6 @@ router = APIRouter()
 class UpdateDataEmployer(BaseModel):
     description: str | None
     photo: str | None  # Фото ссылкой отправляется
-
 
 
 class UpdateDataWorker(BaseModel):
@@ -41,7 +38,6 @@ async def put_employer(
     )
 
 
-
 @router.put("/worker")
 async def put_worker(
     worker: Annotated[GetWorkerByTokenResult, Depends(check_auth_worker_token)],
@@ -49,18 +45,9 @@ async def put_worker(
 ):
     await update_worker(
         edgedb_client,
-<<<<<<< HEAD
         id=worker.id,
         bio=update_data.description,
         photo=update_data.photo,
         resume=update_data.resume,
         skills=update_data.skills,
     )
-=======
-        id = worker.id,
-        bio = update_data.description,
-        photo = update_data.photo,
-        resume = update_data.resume,
-        skills = update_data.skills
-        )
->>>>>>> 78fdfc5626ef83b0013af58cc6d423c5570d4c05
