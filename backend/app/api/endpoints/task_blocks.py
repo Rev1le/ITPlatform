9bcdf203c953bd0c9fca23c77690b13d0ce5f31b
Rev1le <r.nikiy@yandex.ru>
2023-04-15 18:09:22 +0500

    Work

---
+++ backend/app/api/endpoints/task_blocks.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from app.core.database import edgedb_client
+
+router = APIRouter()
+
+@router.get("/")
+def get_all_tasks_blocks():
+    pass
--- backend/app/api/endpoints/vacancy.py
+++ backend/app/api/endpoints/vacancy.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
+from app.core.database import edgedb_client
+from app.queries.get_vacancies_async_edgeql import get_vacancies
+
 router = APIRouter()
 
 
 @router.get("/")
-def get_all_vacancies():
-    # select * from vacancy
-    ...
+async def get_all_vacancies():
+    return await get_vacancies(edgedb_client)
